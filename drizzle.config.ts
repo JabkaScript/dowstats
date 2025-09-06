4cd20fa9622ede525a80d8a37738789911a17667
jabkascript <490vbt@gmail.com>
2025-09-06 15:27:04 +0300

    refactor(db): simplify db env variable fallback chain
    Remove direct DB_* env variables in favor of NUXT_* variants to maintain consistency with Nuxt environment variable naming convention

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   out: 'server/database',
   dialect: 'mysql',
   dbCredentials: {
-    host: process.env.DB_HOST || process.env.NUXT_DB_HOST || process.env.NUXT_PUBLIC_DB_HOST!,
-    port: Number(process.env.DB_PORT || process.env.NUXT_DB_PORT || process.env.NUXT_PUBLIC_DB_PORT) || 3306,
-    user: process.env.DB_USER || process.env.NUXT_DB_USER || process.env.NUXT_PUBLIC_DB_USER!,
-    password: process.env.DB_PASSWORD || process.env.NUXT_DB_PASSWORD || process.env.NUXT_PUBLIC_DB_PASSWORD!,
-    database: process.env.DB_NAME || process.env.NUXT_DB_NAME || process.env.NUXT_PUBLIC_DB_NAME!,
+    host:  process.env.NUXT_DB_HOST || process.env.NUXT_PUBLIC_DB_HOST!,
+    port: Number(process.env.NUXT_DB_PORT || process.env.NUXT_PUBLIC_DB_PORT) || 3306,
+    user: process.env.NUXT_DB_USER || process.env.NUXT_PUBLIC_DB_USER!,
+    password: process.env.NUXT_DB_PASSWORD || process.env.NUXT_PUBLIC_DB_PASSWORD!,
+    database: process.env.NUXT_DB_NAME || process.env.NUXT_PUBLIC_DB_NAME!,
   },
 })
