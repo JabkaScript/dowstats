fbfb00f8272212008eeaf911ecffd59bc2b7f48b
jabkascript <490vbt@gmail.com>
2025-09-14 00:34:10 +0300

    refactor(player): remove unused tabs component from player page

--- app/pages/player/[id].vue
+++ app/pages/player/[id].vue
@@ -2,24 +2,24 @@
 const route = useRoute()
 const sid = route.params.id
 const { statsType } = storeToRefs(useFiltersStore())
-const items = computed(() => {
-  return [
-    {
-      label: 'Relic',
-      value: 'relic',
-    },
-    {
-      label: 'Dow Stats',
-      value: 'dowstats',
-      disabled: true,
-    },
-  ]
-})
+// const items = computed(() => {
+//   return [
+//     {
+//       label: 'Relic',
+//       value: 'relic',
+//     },
+//     {
+//       label: 'Dow Stats',
+//       value: 'dowstats',
+//       disabled: true,
+//     },
+//   ]
+// })
 </script>
 <template>
   <UContainer>
     <UPage>
-      <UTabs v-model="statsType" variant="link" class="w-fit" :content="false" :items />
+      <!-- <UTabs v-model="statsType" variant="link" class="w-fit" :content="false" :items /> -->
       <PlayerRelic v-if="statsType === 'relic'" :sid />
     </UPage>
   </UContainer>
